3787388c06c270fc27c4e33ebe2ce7a365424271
Evgeny Gorodetskiy <evsgor@gmail.com>
2026-03-02 22:34:21 +0300

    Add Windows SDK version 10.0.26100 to FindWindowsSDK.cmake

--- Externals/CMakeModules.cmake
+++ Externals/CMakeModules.cmake
@@ -1,7 +1,7 @@
 CPMAddPackage(
     NAME CMakeModules
     GITHUB_REPOSITORY MethanePowered/CMakeModules
-    GIT_TAG 09b04c9d113cd3a5ed49711999de68896e352c60 # last commit from 'methane' branch
+    GIT_TAG 471d10b21f836b8a59119b60b28aae67e2a51b8e # last commit from 'methane' branch
     DOWNLOAD_ONLY YES
 )
 
