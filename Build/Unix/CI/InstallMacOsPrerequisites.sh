8a3cdd57a2118aa80e6ae59321ed05f51be3bdb7
Evgeny Gorodetskiy <evsgor@gmail.com>
2026-06-30 17:51:03 +0300

    Download Metal toolchain on MacOS only when XCode version >= 26.0

--- Build/Unix/CI/InstallMacOsPrerequisites.sh
+++ Build/Unix/CI/InstallMacOsPrerequisites.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # CI helper script to install MacOS packages, MethaneKit build prerequisites.
-xcodebuild -downloadComponent MetalToolchain
+
+version_ge() {
+    [ "$(printf '%s\n' "$1" "$2" | sort -V | head -n1)" != "$1" ] || [ "$1" = "$2" ]
+}
+
+# Minimum XCode version since which xcodebuild has command -downloadComponent
+XCODE_CURRENT_VERSION=$(xcodebuild -version | grep "Xcode" | awk '{print $2}')
+echo "XCode version: $XCODE_CURRENT_VERSION"
+
+if version_ge "$XCODE_CURRENT_VERSION" "26.0"; then
+    echo "Downloading Metal toolchain..."
+    xcodebuild -downloadComponent MetalToolchain
+fi
