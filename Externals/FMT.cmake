e6cbe819e31da1cb1acba01b13a0aac6fcfc5471
Evgeny Gorodetsky <evsgor@gmail.com>
2026-08-09 17:27:41 +0300

    Disable MSVC warning in FMT header, when built with VS2022

--- Externals/FMT.cmake
+++ Externals/FMT.cmake
@@ -9,3 +9,11 @@ set_target_properties(fmt
     PROPERTIES
     FOLDER Externals
 )
+
+if(MSVC AND # For VS2022 build disable warnings
+   CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "19.30" AND
+   CMAKE_CXX_COMPILER_VERSION VERSION_LESS "19.50") 
+    target_compile_options(fmt PUBLIC
+        /wd4127 # conditional expression is constant 
+    )
+endif()
